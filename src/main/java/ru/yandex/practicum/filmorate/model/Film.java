b19ecbe88d2bcf199a63246dee09a219d00adce6
Valentina Svistunova <svistunova.vv@yandex.com>
2025-03-24 15:06:22 +0300

    Added Spring Boot; Added Controllers

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,56 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.Instant;
+
+
+@Data
 public class Film {
+    public static final int MAX_DESCRIPTION_SIZE = 200;
+
+    Long id;
+    String name;
+    String description;
+    Instant releaseDate;
+    Duration duration;
+
+    public void validate() throws ValidationException {
+        validateName();
+        validateDescription();
+        validateReleaseDate();
+        validateDuration();
+    }
+
+    private void validateDuration() {
+        if (duration == null)
+            throw new ValidationException("Продолжительность должна быть положительным числом");
+
+        if (duration.isZero() || duration.isNegative())
+            throw new ValidationException("Продолжительность должна быть положительным числом");
+    }
+
+    private void validateReleaseDate() {
+        if (releaseDate == null)
+            throw new ValidationException("Дата релиза не задана");
+
+        Instant instant = Instant.parse("1895-12-28T00:00:00.00Z");
+        if (releaseDate.isBefore(instant))
+            throw new ValidationException("Дата релиза не может быть ранее " + instant);
+    }
+
+    private void validateName() {
+        if (name == null)
+            throw new ValidationException("Название не может быть пустым");
+
+        if (name.isBlank())
+            throw new ValidationException("Название не может быть пустым");
+    }
+
+    private void validateDescription() {
+        if (description != null && description.length() > MAX_DESCRIPTION_SIZE)
+            throw new ValidationException("Максимальная длина описания - " + MAX_DESCRIPTION_SIZE + " символов");
+    }
 }
