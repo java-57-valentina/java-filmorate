64357114ca3bedda9ac9e2161a8daee37020b095
Valentyna Svistunova <svistunova.valentyna@gmail.com>
2025-03-25 14:12:24 +0300

    Fixed tests

--- src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
-    User createUser(String login, String name, String email, Instant bd) {
+    User createUser(String login, String name, String email, LocalDate bd) {
         User user = new User();
         user.setName(name);
         user.setLogin(login);
@@ -23,70 +24,70 @@ class UserTest {
     @Test
     void validate_ok() {
         User user = createUser("login", "name", "email@e.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertDoesNotThrow(user::validate);
     }
 
     @Test
     void validate_null_name() {
         User user = createUser("login", null, "email@e.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertDoesNotThrow(user::validate);
     }
 
     @Test
     void validate_null_login() {
         User user = createUser(null, "name", "email@e.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_bad_login() {
         User user = createUser("the login", "name", "email@e.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_empty_login() {
         User user = createUser("", "name", "email@e.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_null_email() {
         User user = createUser("login", "name", null,
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_empty_email() {
         User user = createUser("login", "name", "",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_bad_email_1() {
         User user = createUser("login", "name", "@dot.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_bad_email_2() {
         User user = createUser("login", "name", "test@",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
     @Test
     void validate_bad_email_3() {
         User user = createUser("login", "name", "test.dot.com",
-                Instant.parse("2005-01-01T00:00:00.63Z"));
+                LocalDate.parse("2005-01-01"));
         assertThrows(ValidationException.class, user::validate);
     }
 
@@ -98,17 +99,17 @@ class UserTest {
 
     @Test
     void validate_bad_birthdate() {
-        Instant instant = Instant.now().plus(1, ChronoUnit.DAYS);
-        User user = createUser("login", null, "email@e.com", instant);
+        LocalDate localDate = LocalDate.now().plus(1, ChronoUnit.DAYS);
+        User user = createUser("login", null, "email@e.com", localDate);
         assertThrows(ValidationException.class, user::validate);
     }
 
 
     @Test
     void testEquals() {
-        Instant instant = Instant.now();
-        User user1 = createUser("login", "name", "email@dot.com", instant);
-        User user2 = createUser("login", "name", "email@dot.com", instant);
+        LocalDate now = LocalDate.now();
+        User user1 = createUser("login", "name", "email@dot.com", now);
+        User user2 = createUser("login", "name", "email@dot.com", now);
 
         assertEquals(user1, user2);
     }
