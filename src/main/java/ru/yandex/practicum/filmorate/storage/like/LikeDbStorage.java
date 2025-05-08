be34578870dd5d48898572956c791c1015dcac29
Valentina Svistunova <svistunova.vv@yandex.com>
2025-05-08 22:58:35 +0300

    Removed redundant classed for mapping of likes

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
@@ -4,13 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.model.Like;
 import ru.yandex.practicum.filmorate.storage.base.BaseStorage;
 
 
 @Slf4j
 @Repository
-public class LikeDbStorage extends BaseStorage<Like> implements LikeStorage {
+public class LikeDbStorage extends BaseStorage<Void> implements LikeStorage {
 
     private static final String SQL_CHECK_LIKE_EXISTS =
             "SELECT COUNT(*) > 0 FROM likes WHERE film_id = ? AND user_id = ?";
@@ -24,8 +23,8 @@ public class LikeDbStorage extends BaseStorage<Like> implements LikeStorage {
             """;
 
     @Autowired
-    public LikeDbStorage(JdbcTemplate jdbcTemplate, LikeRowMapper rowMapper) {
-        super(jdbcTemplate, rowMapper);
+    public LikeDbStorage(JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate, (rs, rowNum) -> null);
     }
 
     @Override
