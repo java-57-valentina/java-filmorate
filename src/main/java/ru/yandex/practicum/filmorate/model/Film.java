4d18cde36846ed6c51634f766b6f9014b987d5e9
Valentina Svistunova <svistunova.vv@yandex.com>
2025-05-08 22:40:53 +0300

    Removed in-memory storages

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,20 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.yandex.practicum.filmorate.exception.AlreadyLikedException;
-import ru.yandex.practicum.filmorate.exception.LikeNotFoundException;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 
 @Data
 @NoArgsConstructor
 public class Film {
-    public static final int MAX_DESCRIPTION_SIZE = 200;
 
     private Long id;
     private String name;
@@ -24,7 +19,6 @@ public class Film {
     private Integer duration;
     private Mpa mpa;
 
-    private Set<Long> likes = new LinkedHashSet<>();
     private Collection<Genre> genres = List.of();
 
     public Film(String name, String description, Integer duration, LocalDate releaseDate, Mpa mpa) {
@@ -33,19 +27,5 @@ public class Film {
         this.duration = duration;
         this.releaseDate = releaseDate;
         this.mpa = mpa;
-    }
-
-    public void addLike(Long userId) {
-        if (!likes.add(userId))
-            throw new AlreadyLikedException(id, userId);
-    }
-
-    public void removeLike(Long userId) {
-        if (!likes.remove(userId))
-            throw new LikeNotFoundException(id, userId);
-    }
-
-    public void clearLikes() {
-        likes.clear();
     }
 }
